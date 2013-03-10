504776fc9a18031bb49d160a776b9b89077c0654
Klaus Purer <klaus.purer@gmail.com>
2013-03-10 17:30:34 +0100

    Added check for automated review on ventral.org.

--- scraper.php
+++ scraper.php
@@ -6,7 +6,8 @@ require 'user_password.php';
 use Goutte\Client;
 
 // "Needs work" issue status ID.
-const PROJECAPP_SCRAPER_NEEDS_WORK = 13;
+const PROJECTAPP_SCRAPER_NEEDS_WORK = 13;
+const PROJECTAPP_SCRAPER_NEEDS_REVIEW = 8;
 
 // Perform a user login.
 $client = new Client();
@@ -22,7 +23,7 @@ if ($login_errors->count() > 0) {
 }
 
 // Get all "needs review" issues.
-$crawler = $client->request('GET', 'http://drupal.org/project/issues/search/1339220?status[]=8');
+$crawler = $client->request('GET', 'http://drupal.org/project/issues/1339220?status=8');
 $issues = $crawler->filterXPath('//tbody/tr/td[1]/a');
 
 if ($issues->count() == 0) {
@@ -56,12 +57,36 @@ foreach ($links as $link) {
       $comment_form = $issue_page->selectButton('Save')->form();
       $comment = 'Git repository URL is missing in the issue summary.';
       $client->submit($comment_form, array(
-        'sid' => PROJECAPP_SCRAPER_NEEDS_WORK,
+        'sid' => PROJECTAPP_SCRAPER_NEEDS_WORK,
         'comment' => $comment,
       ));
       continue;
     }
   }
 
+  // Get the issue summary + all commments.
+  $issue_thread =  $issue_page->filter('#content');
+  // Check if someone already posted a link to ventral.org automated reviews.
+  $issue_links = $issue_thread->filterXPath("//@href[starts-with(., 'http://ventral.org/pareview')]");
+  if ($issue_links->count() == 0) {
+    // Invoke pareview.sh now to check automated review errors.
+    $pareview_output = array();
+    exec('pareview.sh ' . escapeshellarg($git_url), $pareview_output);
+    // If there are more than 10 lines output then we assume that some errors
+    // should be fixed.
+    if (count($pareview_output) > 10) {
+      $comment_form = $issue_page->selectButton('Save')->form();
+      $comment = 'There are some errors reported by automated review tools, did you already check them? See http://ventral.org/pareview/' . str_replace(array('/', '.', ':'), '', $git_url);
+      $client->submit($comment_form, array(
+        'sid' => PROJECTAPP_SCRAPER_NEEDS_WORK,
+        'comment' => $comment,
+      ));
+      continue;
+    }
+  }
 
+  // Post a hint to the review bonus program.
+  $issue_text = $issue_thread->text();
+  //if (stripos($issue_text, 'review bonus'))
+  //print_r($issue_text);
 }
