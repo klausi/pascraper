6b82e1b9f4f3901e645809af5c98afffb24e31c2
Klaus Purer <klaus.purer@gmail.com>
2013-03-11 22:40:11 +0100

    Put all detected errors together in one single comment post.

--- scraper.php
+++ scraper.php
@@ -38,6 +38,10 @@ $links = $issues->links();
 foreach ($links as $link) {
   $issue_page = $client->click($link);
   $issue_summary = $issue_page->filter('.node-content');
+  // Initialize empty comment list that shoudl be posted.
+  $post = array();
+  // Do not touch the issue's status per default.
+  $status = NULL;
 
   // Extract all links out of the issue summary to determine the Git clone URL
   // from the project page link.
@@ -62,9 +66,8 @@ foreach ($links as $link) {
     preg_match('/http:\/\/git\.drupal\.org\/sandbox\/.+\.git|[^\s]+@git\.drupal\.org:sandbox\/.+\.git|http:\/\/drupalcode\.org\/sandbox\/.+.git|git\.drupal\.org:sandbox\/.+\.git/', $text, $matches);
     if (empty($matches)) {
       // Set the issue to "needs work" as the link to the project page is missing.
-      $comment = 'Link to the project page is missing in the issue summary, please add it.';
-      projectapp_scraper_post_comment($issue_page, $comment, PROJECTAPP_SCRAPER_NEEDS_WORK);
-      continue;
+      $post[] = 'Link to the project page and git clone command are missing in the issue summary, please add them.';
+      $status = PROJECTAPP_SCRAPER_NEEDS_WORK;
     }
     else {
       $url = $matches[0];
@@ -74,33 +77,36 @@ foreach ($links as $link) {
   }
 
   // Get the issue summary + all commments.
-  $issue_thread =  $issue_page->filter('#content');
-  // Check if someone already posted a link to ventral.org automated reviews.
-  $issue_links = $issue_thread->filterXPath("//@href[starts-with(., 'http://ventral.org/pareview')]");
-  if ($issue_links->count() == 0) {
-    // Invoke pareview.sh now to check automated review errors.
-    $pareview_output = array();
-    $return_var = 0;
-    exec('pareview.sh ' . escapeshellarg($git_url), $pareview_output, $return_var);
-    if ($return_var == 1) {
-      print 'Git clone failed for ' . $git_url . ', issue: ' . $client->getRequest()->getUri();
-      continue;
-    }
-    // If there are more than 10 lines output then we assume that some errors
-    // should be fixed.
-    if (count($pareview_output) > 10) {
-      $comment = 'There are some errors reported by automated review tools, did you already check them? See http://ventral.org/pareview/' . str_replace(array('/', '.', ':'), '', $git_url);
-      projectapp_scraper_post_comment($issue_page, $comment, PROJECTAPP_SCRAPER_NEEDS_WORK);
-      continue;
+  $issue_thread = $issue_page->filter('#content');
+
+  if ($git_url) {
+    // Check if someone already posted a link to ventral.org automated reviews.
+    $issue_links = $issue_thread->filterXPath("//@href[starts-with(., 'http://ventral.org/pareview')]");
+    if ($issue_links->count() == 0) {
+      // Invoke pareview.sh now to check automated review errors.
+      $pareview_output = array();
+      $return_var = 0;
+      exec('pareview.sh ' . escapeshellarg($git_url), $pareview_output, $return_var);
+      if ($return_var == 1) {
+        print 'Git clone failed for ' . $git_url . ', issue: ' . $client->getRequest()->getUri();
+      }
+      // If there are more than 30 lines output then we assume that some errors
+      // should be fixed.
+      elseif (count($pareview_output) > 30) {
+        $post[] = 'There are some errors reported by automated review tools, did you already check them? See http://ventral.org/pareview/' . str_replace(array('/', '.', ':'), '', $git_url);
+        $status = PROJECTAPP_SCRAPER_NEEDS_WORK;
+      }
     }
   }
 
   // Post a hint to the review bonus program.
   $issue_text = $issue_thread->text();
   if (stripos($issue_text, 'review bonus') === FALSE) {
-    $comment = 'We are currently quite busy with all the project applications and I can only review projects with a <a href="http://drupal.org/node/1410826">review bonus</a>. Please help me reviewing and I\'ll take a look at your project right away :-)';
-    projectapp_scraper_post_comment($issue_page, $comment);
-    continue;
+    $post[] = 'We are currently quite busy with all the project applications and I can only review projects with a <a href="http://drupal.org/node/1410826">review bonus</a>. Please help me reviewing and put yourself on the <a href="http://drupal.org/project/issues/search/projectapplications?status[]=8&status[]=14&issue_tags=PAReview%3A+review+bonus">PAReview: review bonus high priority list</a>. Then I\'ll take a look at your project right away :-)';
+  }
+
+  if (!empty($post)) {
+    projectapp_scraper_post_comment($issue_page, $post, $status);
   }
 }
 
@@ -108,14 +114,14 @@ foreach ($links as $link) {
  * Helper function to either output the issue comment on a dry-run or post a new
  * comment to the issue.
  */
-function projectapp_scraper_post_comment($issue_page, $comment, $status = NULL) {
+function projectapp_scraper_post_comment($issue_page, $post, $status = NULL) {
   global $argv;
   global $client;
   if (isset($argv[1]) && $argv[1] == 'dry-run') {
     // Dry run, so just print out the suggested comment.
     $output = array(
       'issue' => $client->getRequest()->getUri(),
-      'comment' => $comment,
+      'comment' => $post,
       'status' => $status,
     );
     print_r($output);
@@ -123,6 +129,7 @@ function projectapp_scraper_post_comment($issue_page, $comment, $status = NULL) 
   else {
     // Production run: post the comment to the drupal.org issue.
     $comment_form = $issue_page->selectButton('Save')->form();
+    $comment = implode("\n\n", $post);
     $form_values = array('comment' => $comment);
     if ($status) {
       $form_values['sid'] = $status;
