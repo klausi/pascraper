d531b0330ce2f7dcc469987bfe9b4b1e7d7733b3
Klaus Purer <klaus.purer@gmail.com>
2013-04-20 01:06:05 +0200

    Changed review bonus link.

--- scraper.php
+++ scraper.php
@@ -161,7 +161,7 @@ COMMENT;
   // Post a hint to the review bonus program.
   $issue_text = $issue_thread->text();
   if (stripos($issue_text, 'review bonus') === FALSE) {
-    $post[] = 'We are currently quite busy with all the project applications and we prefer projects with a <a href="http://drupal.org/node/1410826">review bonus</a>. Please help reviewing and we will take a look at your project right away :-)';
+    $post[] = 'We are currently quite busy with all the project applications and we prefer projects with a <a href="http://drupal.org/node/1975228">review bonus</a>. Please help reviewing and we will take a look at your project right away :-)';
     $post[] = 'Also, you should get your friends, colleagues or other community members involved to review this application. Let them go through the <a href="http://drupal.org/node/1587704">review checklist</a> and post a comment that sets this issue to "needs work" (they found some problems with the project) or "reviewed & tested by the community" (they found no major flaws).';
   }
 
